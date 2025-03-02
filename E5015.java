ef571b5dbebd67c5ec75fdfd15eb2c2ecf7ee3b8
Sadra <sadrarezaei4@gmail.com>
2025-03-02 15:04:43 +0330

    first commit

---
+++ E5015.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class E5015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+
+        if (num <= 50000) {
+            System.out.println(num/100);
+        }
+        else if (num <= 75000) {
+            System.out.println(num * 2 /100);
+        }
+        else if (num <= 100000) {
+            System.out.println(num * 3 /100);
+        }
+        else if (num <= 250000) {
+            System.out.println(num * 4 / 100);
+        }
+        else if (num <= 500000) {
+            System.out.println(num * 5 / 100);
+        }
+        else {
+            System.out.println(num * 6 / 100);
+        }
+    }
+}
