90c75bca22dbef87c18fe86e715a3349b2d85f42
Sadra <sadrarezaei4@gmail.com>
2025-09-07 07:44:09 +0330

    FPR_1-4

---
+++ ap/projects/finalproject/models/Loan.java
@@ -0,0 +1,28 @@
+package ap.projects.finalproject.models;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Loan implements Serializable {
+    private static final long serialVersionUID = 1L;
+    public String id;
+    public String bookId;
+    public String studentUsername;
+    public LocalDate startDate;
+    public LocalDate endDate;
+    public boolean approved = false;
+    public LocalDate receivedDate = null;
+    public LocalDate returnedDate = null;
+
+    public Loan(String id, String bookId, String studentUsername, LocalDate start, LocalDate end){
+        this.id = id; this.bookId = bookId; this.studentUsername = studentUsername;
+        this.startDate = start; this.endDate = end;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Loan[%s] book=%s student=%s %s->%s approved=%s received=%s returned=%s",
+                id, bookId, studentUsername, startDate, endDate, approved, receivedDate, returnedDate);
+    }
+}
+
